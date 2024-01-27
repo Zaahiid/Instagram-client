02615c506d764aa88783546974d73ffe37d6e75d
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-28 00:21:12 +0530

    added Auth and AuthForm

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Image } from "@chakra-ui/image";
+import { Box, Container, Flex, VStack } from "@chakra-ui/layout";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const Auth = () => {
   return (
-    <div>Auth</div>
-  )
-}
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          {/* Left side */}
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="Phone Image" />
+          </Box>
+          {/* Right side */}
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
+            <Box textAlign={"center"}>Get the App</Box>
+            <Flex gap={5} justifyContent={"center"}>
+              <Image src="/playstore.png" h={10} alt="Play store" />
+              <Image src="/microsoft.png" h={10} alt="Microsoft store" />
+            </Flex>
+          </VStack>
+        </Flex>
+      </Container>
+    </Flex>
+  );
+};
 
-export default Auth
+export default Auth;
