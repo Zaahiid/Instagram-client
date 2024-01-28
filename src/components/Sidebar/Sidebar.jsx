82ab3942756b7f2ae486924071008da201f3f6d5
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-28 22:54:00 +0530

    added responsive logo in the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,45 @@
 import { Flex, Box } from "@chakra-ui/layout";
-import { Link } from '@chakra-ui/react'
-import {Link as RouterLink} from 'react-router-dom'
+import { Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
 
 const Sidebar = () => {
   return (
     <Box
-    height={"100vh"}
-    borderRight={"1px solid"}
-    borderColor={"whiteAlpha.300"}
-    py={8}
-    position={"sticky"}
-    top={0}
-    left={0}
-    px={{base:1,md:4}}
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 1, md: 4 }}
     >
-    <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
-        <Link to={"/"} as={RouterLink}></Link>
-    </Flex>
+      <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor={"pointer"}
+        >
+            <InstagramLogo/>
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          cursor={"pointer"}
+          borderRadius={6}
+          _hover={{bg:"whiteAlpha.200"}}
+          w={10}
+        >
+            <InstagramMobileLogo/>
+        </Link>
+      </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
