763243b777306e9c3324f3ef0dd8b0761b3171b0
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-29 23:10:46 +0530

    Defined the layout for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,16 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid blue"}>
+          Feedpost
+        </Box>
+        <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"} border={"1px solid red"}>
+          Suggested User
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
