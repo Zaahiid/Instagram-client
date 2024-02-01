f213d5d650213b003a0892635a0cbafe6d9b19de
Zaahiid <zahidrsheikh01@gmail.com>
2024-02-01 23:52:58 +0530

    added comment box

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,31 +1,81 @@
 import { useState } from "react";
-import { Box, Flex } from "@chakra-ui/react"
-import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import {
+  CommentLogo,
+  NotificationsLogo,
+  UnlikeLogo,
+} from "../../assets/constants";
 
 const PostFooter = () => {
-    const [liked, setLiked] = useState(false);
-    const [likes, setLikes] = useState(1000);
-    const handleLike = () => {
-        if(liked){
-            setLiked(false);
-            setLikes(likes-1);
-        }else{
-            setLiked(true);
-            setLikes(likes+1);
-        }
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
     }
+  };
   return (
-    <>
-    <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+    <Box mb={10}>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-            {!liked ? (<NotificationsLogo/>) : (<UnlikeLogo/>)}
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
         </Box>
-        <Box cursor={"pointer"} fontSize={17}>
-            <CommentLogo/>
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
         </Box>
-    </Flex>
-    </>
-  )
-}
+      </Flex>
+      <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+      </Text>
+      <Text fontWeight={700} fontSize={"sm"}>
+        Zaahiid
+        <Text as="span" fontWeight={400}>
+          Feeling Good
+        </Text>
+      </Text>
+      <Text fontSize={"sm"} color={"gray"}>
+        view all 1,000 comments
+      </Text>
+      <Flex
+        alignItems={"center"}
+        gap={2}
+        justifyContent={"space-between"}
+        w={"full"}
+      >
+        <InputGroup>
+          <Input
+            variant={"flushed"}
+            placeholder="Add a comment..."
+            fontSize={14}
+          />
+          <InputRightElement>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={500}
+              cursor={"pointer"}
+                _hover={{color:"white"}}
+                bg={"transparent"}
+            >
+              Post
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </Box>
+  );
+};
 
-export default PostFooter
+export default PostFooter;
